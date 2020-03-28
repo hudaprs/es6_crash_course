e9148a1a0612988cb72ead51a922297dc32a96cb
John Doe <huda.prs2002@gmail.com>
2020-03-28 12:13:42 +0700

    number and string method

---
+++ 03-string-number-methods/index.js
@@ -0,0 +1,30 @@
+/**
+ * String
+ * string.startsWith
+ * string.endsWith
+ * string.includes
+ */
+
+ let string = 'Hello, my name is Huda Prasetyo and I love JavaScript';
+
+ console.log(string.startsWith('Hello'));
+ console.log(string.endsWith('JavaScript'));
+ console.log(string.includes('Huda'));
+ 
+
+ /**
+  * Number
+  * 0x = Hexadecimal
+  * 0b = Binary
+  * 0o = Octal
+  */
+ console.log(0x1231)
+ console.log(0b10001000)
+ console.log(0o123)
+
+ /**
+  * Number Method
+  */
+ console.log(Number.isFinite(NaN));
+ console.log(Number.isNaN('string'));
+ console.log(Number.isInteger(Infinity));
